faa37d2adecc3a0ac9174358226fdf2555682c5e
kiran babu <kiranbabubandela6@gmail.com>
2025-10-29 14:41:15 +0530

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+import os
 
 # ----------------------------------------------------
 # 🎯 Page Setup
